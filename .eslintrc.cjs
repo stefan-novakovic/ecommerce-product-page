00d62ef8c988abc451acd14c4d9e8559dcce6e6f
Stefan Novaković <snovakovic3@gmail.com>
2024-07-25 17:23:14 +0200

    Initial commit (boilerplate cleanup + eslint and prettier config)

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+   root: true,
+   env: { browser: true, es2020: true },
+   extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:import/recommended',
+      'plugin:jsx-a11y/recommended',
+      'eslint-config-prettier'
+   ],
+   ignorePatterns: ['dist', '.eslintrc.cjs'],
+   parser: '@typescript-eslint/parser',
+   plugins: ['react-refresh'],
+   rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': [
+         'error',
+         {
+            vars: 'all',
+            args: 'after-used',
+            ignoreRestSiblings: true,
+            argsIgnorePattern: '^_'
+         }
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off'
+   }
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+   const stef = 'sste';
+   return (
+      <div className="App">
+         <h1>Hello</h1>
+      </div>
+   );
+}
+
+export default App;
